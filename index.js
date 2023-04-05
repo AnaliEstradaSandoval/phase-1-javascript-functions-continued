e47cbc04aab697c9a6476a2e585466d1ebcb94c5
Anali Estrada Sandoval <analiestrada44@gmail.com>
2023-04-05 14:48:36 -0700

    Functions continued lab completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity){
+    if (activity){
+        return `This Saturday, I want to ${activity}!`
+    } else{
+    return "This Saturday, I want to roller-skate!"
+    }
+}
+
+const mondayWork = function(activity){
+    if (activity){
+        return `This Monday, I will ${activity}.`
+    }else {
+        return 'This Monday, I will go to the office.'
+    }
+}
+
+function wrapAdjective(part1 = "*") {
+    return function (part2 = "special"){
+        return `You are ${part1}${part2}${part1}!`
+    } ;
+}
